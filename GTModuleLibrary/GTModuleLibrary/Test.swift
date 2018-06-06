c48fba7333d79d7098ec4d29ef19b4ed019426d9
liuxc123 <lxc_work@126.com>
2018-06-06 17:00:00 +0800

    提交

---
+++ GTModuleLibrary/GTModuleLibrary/Test.swift
@@ -0,0 +1,19 @@
+//
+//  Test.swift
+//  GTModuleLibrary
+//
+//  Created by liuxc on 2018/6/6.
+//  Copyright © 2018年 liuxc. All rights reserved.
+//
+
+import UIKit
+
+class Test: NSObject {
+
+    override init() {
+        super.init()
+
+        kStatusBarHeight()
+    }
+
+}
