75da5d8365f76a7c76e1bd89add3bae9109ede85
liuxc123 <lxc_work@126.com>
2018-06-14 09:42:08 +0800

    增加高德地图工具类

---
+++ GTModuleLibrary/GTModuleLibrary/GTBaseModule/MAMapCore/MAMapCore.h
@@ -0,0 +1,13 @@
+//
+//  MAMapCore.h
+//  GTModuleLibrary
+//
+//  Created by liuxc on 2018/6/14.
+//  Copyright © 2018年 liuxc. All rights reserved.
+//
+
+#ifndef MAMapCore_h
+#define MAMapCore_h
+
+
+#endif /* MAMapCore_h */
